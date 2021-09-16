24480d2e8bc1fdfa73ec37381e99f1cf2d8ad885
cplan082-tech <cplan082@uottawa.ca>
2021-09-15 23:06:47 -0400

    Trimmed down original temp_sense script. Added date/time stamp for each data selection. Tupple created with (sensor name,temp, humdity,  datetime)

--- Software/Tool/temp_sens.py
+++ Software/Tool/temp_sens.py
@@ -4,29 +4,23 @@ import sys
 import argparse
 import time
 import Adafruit_DHT
+from datetime import datetime
 
+sensor_name = "Temp/Humidity"
 sensor = Adafruit_DHT.DHT11
 
 # GPIO14 (pin 8) on pi
 data_pin = 14
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--fahrenheit", help="output temperature in Fahrenheit", action="store_true")
-
-    return parser.parse_args()
-
-def celsius_to_fahrenheit(degrees_celsius):
-        return (degrees_celsius * 9/5) + 32
 
 def main():
-    args = parse_args()
 
     while True:
         try:
             # Gather the humidity and temperature
-            # data from the sensor; GPIO Pin 4
+            # data from the sensor; GPIO Pin 14
             humidity, temperature = Adafruit_DHT.read_retry(sensor, data_pin)
+            stamp = datetime.now()
 
         except RuntimeError as e:
             # GPIO access may require sudo permissions
@@ -38,10 +32,10 @@ def main():
             time.sleep(2.0)
             continue
 
-        if args.fahrenheit:
-            print("Temp: {0:0.1f}*F, Humidity: {1:0.1f}%".format(celsius_to_fahrenheit(temperature), humidity))
-        else:
-            print("Temp:{0:0.1f}*C, Humidity: {1:0.1f}%".format(temperature, humidity))
+        package = (sensor_name, temperature, humidity, stamp)
+       
+        #print("Temp:{0:0.1f}*C, Humidity: {1:0.1f}%".format(temperature, humidity))
+        print(package)
 
         time.sleep(2.0)
 
