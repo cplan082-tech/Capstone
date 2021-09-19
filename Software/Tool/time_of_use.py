3dc2cf21857d4e22f104c908d0a610d3b7a12551
cplan082-tech <cplan082@uottawa.ca>
2021-09-19 00:10:26 -0400

    fixed compile bugs

--- Software/Tool/time_of_use.py
+++ Software/Tool/time_of_use.py
@@ -7,15 +7,25 @@ gpio.setmode(gpio.BCM)
 gpio.setup(tool_trig, gpio.IN)
 
 
-def start_tou(pin):
-    start_time = datetime.now().time()
+# def start_tou(pin):
+#     start_time = datetime.now().time()
 
-def stop_tou(pin):
-    time_of_use = datetime.now().time() - start_time
-    print (f"time of use = {time_of_use}")
+# def stop_tou(pin):
+#     time_of_use = datetime.now().time() - start_time
+#     print (f"time of use = {time_of_use}")
+    
+def event_handler(pin):
+    if pin:
+        start_time = datetime.now().time()
+    elif ~pin:
+        time_of_use = datetime.now().time() - start_time
+        print (f"time of use = {time_of_use}")
+        
 
-gpio.add_event_detect(tool_trig, gpio.RISING, callback=start_tou)
-gpio.add_event_detect(tool_trig, gpio.FALLING, callback=stop_tou)
+gpio.add_event_detect(tool_trig, gpio.BOTH, callback=event_handler)
+
+# gpio.add_event_detect(tool_trig, gpio.RISING, callback=start_tou)
+# gpio.add_event_detect(tool_trig, gpio.FALLING, callback=stop_tou)
 
 try:
     while True:
@@ -23,9 +33,6 @@ try:
 except:
     gpio.cleanup()
 
-    
-    except:
-        gpio.cleanup()
 
 
 
