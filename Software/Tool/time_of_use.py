43fe8e87d2ab5618748d7915afb8c1f9b49c7f84
cplan <cplan082@uottawa.ca>
2021-09-19 00:00:14 -0400

    debuging time_of_use.py. Error Rec: 'RuntimeError: Conflicting edge detection already enabled for this GPIO channel'.

--- Software/Tool/time_of_use.py
+++ Software/Tool/time_of_use.py
@@ -7,12 +7,12 @@ gpio.setmode(gpio.BCM)
 gpio.setup(tool_trig, gpio.IN)
 
 
-def start_tou:
+def start_tou(pin):
     start_time = datetime.now().time()
 
-def stop_tou:
+def stop_tou(pin):
     time_of_use = datetime.now().time() - start_time
-    print f"time of use = {time_of_use}"
+    print (f"time of use = {time_of_use}")
 
 gpio.add_event_detect(tool_trig, gpio.RISING, callback=start_tou)
 gpio.add_event_detect(tool_trig, gpio.FALLING, callback=stop_tou)
@@ -20,8 +20,8 @@ gpio.add_event_detect(tool_trig, gpio.FALLING, callback=stop_tou)
 try:
     while True:
         pass
-    except:
-        gpio.cleanup()
+except:
+    gpio.cleanup()
 
 
 
