5076b30b98e8dbe973b26045aafe6de90cfd07cc
cplan082-tech <cplan082@uottawa.ca>
2021-09-18 23:28:37 -0400

    First draft of time_of_use.py script

---
+++ Software/Tool/time_of_use.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as gpio
+import time
+from datetime import datetime
+
+tool_trig = 21
+gpio.setmode(gpio.BCM)
+gpio.setup(tool_trig, gpio.IN)
+
+
+def start_tou:
+    start_time = datetime.now().time()
+
+def stop_tou:
+    time_of_use = datetime.now().time() - start_time
+    print f"time of use = {time_of_use}"
+
+gpio.add_event_detect(tool_trig, gpio.RISING, callback=start_tou)
+gpio.add_event_detect(tool_trig, gpio.FALLING, callback=stop_tou)
+
+try:
+    while True:
+        pass
+    except:
+        gpio.cleanup()
+
+
+
+
