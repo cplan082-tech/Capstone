dc1e1eee3f4152890361f5042b59925d5f312946
cplan <cplan082@uottawa.ca>
2021-09-22 00:04:21 -0400

    Debugging 'prev_val' bug

--- Software/Tool/time_of_use.py
+++ Software/Tool/time_of_use.py
@@ -13,6 +13,7 @@ def event_handler(pin):
     time.sleep(0.1) # debouncing
     
     if (tool_trig_pin_val == gpio.input(tool_trig_pin)) & (prev_val != tool_trig_pin_val):
+        print('hit')
         if tool_trig_pin_val:
             start_time = datetime.now()
             prev_val = 1
@@ -21,7 +22,11 @@ def event_handler(pin):
             prev_val == 0
             retval = datetime.now() - start_time
             print(f"retval {retval}")
-       
+    else:  # else is only for testing
+        print(f"tool_trig_pin_val: {tool_trig_pin_val}")
+        print(f"gpio.input(tool_trig_pin): {gpio.input(tool_trig_pin)}")
+        print(f"prev_val: {prev_val}")
+        print(f"tool_trig_pin_val: {tool_trig_pin_val}")
         
 gpio.setmode(gpio.BCM)
 gpio.setup(tool_trig_pin, gpio.IN)
