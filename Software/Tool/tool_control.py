cee5e47ac9216f930e71f1453933c49983c0772e
cplan <cplan082@uottawa.ca>
2021-09-22 22:35:05 -0400

    packing data into csv (started)

--- Software/Tool/tool_control.py
+++ Software/Tool/tool_control.py
@@ -3,32 +3,46 @@ import time_of_use
 import time
 import board
 import busio
+# import pandas as pd
 import adafruit_adxl34x
 import Adafruit_DHT
 from datetime import datetime
+# from pandas import DataFrame
+import csv
 
-# Accelorometer Init
+# Accelerometer Init
+name_accel = "Accelerometer"
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer_obj = adafruit_adxl34x.ADXL345(i2c)
 
 # Temp/Humidity sense Init
-sensor_name = "Temp/Humidity"
+name_tempHum = "Temp/Humidity"
 sensor = Adafruit_DHT.DHT11
 
 # GPIO14 (pin 8) on pi
-data_pin = 14
+data_pin_temp = 14
 
 
 try:
     while True:
         time.sleep(0.5)
         accel = accelerometer_obj.acceleration
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, data_pin)
-        print(f"acceleration: {accel}")
-        print(f"Temp: {temperature}")
-        print(f"Hum: {humidity}")
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, data_pin_temp)
+        stamp = datetime.now()
+        package = {'temp': temperature,
+                   'humid': humidity,
+                   'accel x': accel[0],
+                   'accel y': accel[1],
+                   'accel z': accel[2],
+                   'timestamp': stamp}
+#         df = DataFrame(data=package)
+#         print(df)
+#         print(f"acceleration: {accel}")
+#         print(f"Temp: {temperature}")
+#         print(f"Hum: {humidity}")
         
         
         
 except:
+    print("exiting")
     gpio.cleanup()
