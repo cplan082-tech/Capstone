587358755eb1b86ab5fe77acb3d577f8ecf58700
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2021-11-08 09:42:29 -0500

    Cleaned up 'wifi_transponder.py' to not continuously define all of it's functions. Did this by moving function definitions outside of while loop. Updated while loop to run indefinetly (forever) instead of 1000000 times

--- Software/wifi_comms/wifi_transponder.py
+++ Software/wifi_comms/wifi_transponder.py
@@ -59,80 +59,76 @@ if MAC ==1:
 else:
     path = dest_path
     FlagPath = dest_path
+    
 
-
-#Loop will run 100000 times
-i=0
-while i<100000:
-
-    #This function checks if the timmer has set a flag file called flag.csv, then pushes it to the tool memory, then deletes it.
-    def flag():
-        print("flag function running")
-        file = pathlib.Path(FlagPath +"/flag.csv") #Checks if the flag file exists
-        if file.exists ():
-            print("Flag file found")
-            output = pexpect.run("scp " + FlagPath +"/flag.csv pi@"+ IP_src +":"+ src_path, events={'(?i)password':""+ Password +"\n"})
-            print("\nThe output of ssh command: \n%s" %output.decode("utf-8"))
-            time.sleep(1)
-            os.remove(FlagPath + "/flag.csv")
-            print("Flag file deleted\n")
-        else:
-            print("No flag file found\n")
-
-        time.sleep(1)
-
-    i=i+1
-    #This function scp's into the tool and grabs all the tool data dumps and brings it to the hub deleting all other copies
-    def Retreive():
-
-        print ("Running Retreive() function")
-
-        output = pexpect.run("scp pi@"+ IP_src +":"+ src_filename + " "+ path, events={'(?i)password':""+ Password +"\n"})
+#This function checks if the timmer has set a flag file called flag.csv, then pushes it to the tool memory, then deletes it.
+def flag():
+    print("flag function running")
+    file = pathlib.Path(FlagPath +"/flag.csv") #Checks if the flag file exists
+    if file.exists ():
+        print("Flag file found")
+        output = pexpect.run("scp " + FlagPath +"/flag.csv pi@"+ IP_src +":"+ src_path, events={'(?i)password':""+ Password +"\n"})
         print("\nThe output of ssh command: \n%s" %output.decode("utf-8"))
         time.sleep(1)
-        print("Tool memory file retreived")
+        os.remove(FlagPath + "/flag.csv")
+        print("Flag file deleted\n")
+    else:
+        print("No flag file found\n")
+
+    time.sleep(1)
+
+#This function scp's into the tool and grabs all the tool data dumps and brings it to the hub deleting all other copies
+def Retreive():
+
+    print ("Running Retreive() function")
+
+    output = pexpect.run("scp pi@"+ IP_src +":"+ src_filename + " "+ path, events={'(?i)password':""+ Password +"\n"})
+    print("\nThe output of ssh command: \n%s" %output.decode("utf-8"))
+    time.sleep(1)
+    print("Tool memory file retreived")
 
 
-    #List the amount of data dump files by login through ssh
-    def ConnectionTest():
-        flag()
-        print("\nRunning ConnectionTest()\n")
-        
-        output = pexpect.run("ssh pi@" + IP_src +" 'ls "+ src_path +" '", events={'(?i)password':""+ Password +"\n"})
-        print("\nThe output of ssh command: \n%s" %output.decode("utf-8"))
-        #os.system("nick")
-        print("\nConnection established\n")
+#List the amount of data dump files by login through ssh
+def ConnectionTest():
+    flag()
+    print("\nRunning ConnectionTest()\n")
+    
+    output = pexpect.run("ssh pi@" + IP_src +" 'ls "+ src_path +" '", events={'(?i)password':""+ Password +"\n"})
+    print("\nThe output of ssh command: \n%s" %output.decode("utf-8"))
+    #os.system("nick")
+    print("\nConnection established\n")
+    time.sleep(1)
+
+    SignalStrength()
+    Retreive()
+
+
+#Check Wifi Connection Strength in dBm
+def SignalStrength():    
+    if MAC ==1:
+        Wifi = subprocess.check_output(["/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I"], shell=True, stderr=subprocess.DEVNULL)
+        sentence=str(Wifi)
+        s = [float(s) for s in re.findall(r'-?\d+\.?\d*', sentence)]
+        firstnumber = ( "% d" + "dBm.") % s[0]
+        print("The Wifi signal power is: " + str(firstnumber))
+        time.sleep(1)
+       
+
+        #Check if Wifi signal is strong enough
+        if s[0] < float(Signal_Power):
+            print("Wifi signal is to weak, cannot transfer data")
+            time.sleep(1)
+            quit()
+    else:
+        Wifi=[]
+        Wifi = subprocess.check_output(["iwconfig"], shell=True, stderr=subprocess.DEVNULL)
+        sentence=str(Wifi)
+        s = [float(s) for s in re.findall(r'-?\d+\.?\d*', sentence)]
+        firstnumber = ( "% d" + "dBm.") % s[12]
+        print("The Wifi signal power is: " + str(firstnumber))
         time.sleep(1)
 
-        SignalStrength()
-        Retreive()
-
-
-    #Check Wifi Connection Strength in dBm
-    def SignalStrength():    
-        if MAC ==1:
-            Wifi = subprocess.check_output(["/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I"], shell=True, stderr=subprocess.DEVNULL)
-            sentence=str(Wifi)
-            s = [float(s) for s in re.findall(r'-?\d+\.?\d*', sentence)]
-            firstnumber = ( "% d" + "dBm.") % s[0]
-            print("The Wifi signal power is: " + str(firstnumber))
-            time.sleep(1)
-           
-
-            #Check if Wifi signal is strong enough
-            if s[0] < float(Signal_Power):
-                print("Wifi signal is to weak, cannot transfer data")
-                time.sleep(1)
-                quit()
-        else:
-            Wifi=[]
-            Wifi = subprocess.check_output(["iwconfig"], shell=True, stderr=subprocess.DEVNULL)
-            sentence=str(Wifi)
-            s = [float(s) for s in re.findall(r'-?\d+\.?\d*', sentence)]
-            firstnumber = ( "% d" + "dBm.") % s[12]
-            print("The Wifi signal power is: " + str(firstnumber))
-            time.sleep(1)
-            
-
+#Loop will run forever
+while True:          
     ConnectionTest()
     
