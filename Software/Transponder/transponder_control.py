48a9d9cf6747b23b6e74b69022683d563fdc0309
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2021-11-08 20:26:02 -0500

    'transponder_control.py' now creates a 'flag.csv' once before entering the while loop. This is just for testing hte flag portion of 'wifi_transponder.py'

--- Software/Transponder/transponder_control.py
+++ Software/Transponder/transponder_control.py
@@ -13,9 +13,15 @@ path_memory = "/home/pi/Hub/Memory/HubMemory/"
 csv_file = "Tool_Memory_acumulator.csv"
 csv_file_input = "Tool_Memory.csv"
 
+# We just want to create a flag csv, we don't care what's in it
+# FOR TESTING
+csvm.dict_to_csv({'place': 'holder'}, os.path.realpath('../wifi_comms/Memory/') # TODO: remove this
+
 while True:
     
     # Checks if any new data has been recieved
     if os.path.isfile(csv_file_input):
         csvm.csv_concat(path_memory, csv_file) # updates accumulator
         os.remove(path_memory + csv_file_input) # Deletes old data
+        
+     
